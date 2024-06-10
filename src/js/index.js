581321c7b4cf71d859fa40b84a3ac8d5636316bb
MSkrypnyk <mariya0skrypnyk@gmail.com>
2024-06-10 20:35:32 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rows = document.querySelectorAll(".customers__table-title");
+  rows.forEach((row, index) => {
+    const delay = index * 100;
+    setTimeout(() => {
+      row.classList.add("animate-row");
+    }, delay);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchForm = document.getElementById("search-form");
+  searchForm.classList.add("customers__search");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const welcomeTitle = document.querySelector(".main__welcome");
+  welcomeTitle.classList.add("main__welcome");
+});
+document.getElementById("toggleSidebar").addEventListener("click", function () {
+  document.querySelector(".sidebar__menu").classList.toggle("sidebar--shown");
+});
